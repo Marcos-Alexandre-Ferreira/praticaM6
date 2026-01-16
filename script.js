61a0544b531a259b95112432642c84b0a817710f
Marcos Alexandre <marcosferreira.ma94@gmail.com>
2026-01-15 23:39:43 -0300

    incluida a validação do palpite dentro do internvalo definido

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ const inputPalpite = document.getElementById("palpite");
 
 let numeroTentativas = 10;
 document.getElementById("numeroTentativas").textContent = "Número de tentativas restantes: " + numeroTentativas;
+
+function capturarDados() {
+    const inputPalpite = document.getElementById("palpite").value; // Captura o valor do input
+    const palpiteNumero = Number(inputPalpite); // Converte para número real
+    
+    if (isNaN(palpiteNumero) || palpiteNumero < 1 || palpiteNumero > 100) {// Validação do palpite enttre o intervalo definido
+        alert("Por favor, insira um número válido entre 1 e 100.");
+        return;
+    }
+}
