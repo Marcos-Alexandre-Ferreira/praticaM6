87224cc0f8a1d1d48a07726f81ef3728998b0f67
Marcos Alexandre <marcosferreira.ma94@gmail.com>
2026-01-16 00:12:14 -0300

    realizado inserção das dicas e o decremento das tentativas

--- script.js
+++ script.js
@@ -7,11 +7,31 @@ let numeroTentativas = 10;
 document.getElementById("numeroTentativas").textContent = "Número de tentativas restantes: " + numeroTentativas;
 
 function capturarDados() {
-    const inputPalpite = document.getElementById("palpite").value; // Captura o valor do input
-    const palpiteNumero = Number(inputPalpite); // Converte para número real
+    const valorInput = document.getElementById("palpite").value; // Captura o valor do input
+    let palpiteNumero = Number(valorInput); // Converte para número real
     
-    if (isNaN(palpiteNumero) || palpiteNumero < 1 || palpiteNumero > 100) {// Validação do palpite enttre o intervalo definido
+    if (!palpiteNumero || palpiteNumero < 1 || palpiteNumero > 100) {// Validação do palpite entre o intervalo definido
         alert("Por favor, insira um número válido entre 1 e 100.");
         return;
     }
+    
+    if (palpiteNumero === numeroAdivinhacao) {
+        alert("Parabéns! Você adivinhou o número corretamente: " + numeroAdivinhacao);//palpite correto
+        numeroTentativas = 0; // Encerra o jogo
+        //refreshPage(); - precisa finalizar ao encotrar o valor correto
+    } else if (palpiteNumero < numeroAdivinhacao) {
+        alert("O número é maior. Tente novamente."); //palpite abaixo do número correto
+        numeroTentativas--;
+        document.getElementById("numeroTentativas").textContent = "Número de tentativas restantes: " + numeroTentativas;
+    } else {
+        alert("O número é menor. Tente novamente."); //palpite acima do número correto
+        numeroTentativas--;
+        document.getElementById("numeroTentativas").textContent = "Número de tentativas restantes: " + numeroTentativas;
+    }
+    
+    if (numeroTentativas === 0 && palpiteNumero !== numeroAdivinhacao) {
+        alert("Fim do jogo! O número era: " + numeroAdivinhacao);
+    }
+    
+    inputPalpite.value = ""; // Limpa o campo de input após cada tentativa
 }
